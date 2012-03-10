21b0f8afab7a098818e32cc9039d94401ccfea14
Student CS <student@mjolnir.labs.cs.pub.ro>
2012-03-10 14:00:28 +0200

    CDL test

--- android/AWoUSO/src/cdl/android/ui/AWoUSOActivity.java
+++ android/AWoUSO/src/cdl/android/ui/AWoUSOActivity.java
@@ -1,8 +1,13 @@
 package cdl.android.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.FrameLayout.LayoutParams;
 import cdl.android.R;
 
 public class AWoUSOActivity extends Activity {
@@ -12,5 +17,18 @@ public class AWoUSOActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+        
+        Button button = (Button) findViewById(R.id.buton_test);        
+        
+        final Intent bla = new Intent(this, NextActivity.class);
+        
+        button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(bla);
+				
+			}
+		});
     }
 }
