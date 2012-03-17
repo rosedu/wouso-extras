74a79e118f7ae027cc575bd93c59d8782608610d
Dragos Badea <badea.dm@gmail.com>
2012-03-17 02:10:35 +0200

    Created MainMenu.java

---
+++ android/AWoUSO/src/cdl/android/ui/MainMenu.java
@@ -0,0 +1,18 @@
+package cdl.android.ui;
+
+import cdl.android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainMenu extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.mainmenu);
+	}
+
+}
