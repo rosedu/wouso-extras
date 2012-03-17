cb9ef64411a86dc1f14c93aa19b7bd24f84e9334
Mihai Zamfirescu <mihai.zamfirescu93@gmail.com>
2012-03-17 12:57:45 +0200

    Work with layouts, gravity. Added button colors.

--- android/AWoUSO/src/cdl/android/ui/AWoUSOActivity.java
+++ android/AWoUSO/src/cdl/android/ui/AWoUSOActivity.java
@@ -1,7 +1,10 @@
 package cdl.android.ui;
 
 import android.app.Activity;
-import android.content.DialogInterface.OnClickListener;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -12,20 +15,27 @@ public class AWoUSOActivity extends Activity {
     /** Called when the activity is first created. */
 	Button red;
 	Button blue;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+        Drawable red_bg = findViewById(R.id.b_red).getBackground();  
+    	PorterDuffColorFilter red_filter = new PorterDuffColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
+        red_bg.setColorFilter(red_filter);
+        Drawable blue_bg = findViewById(R.id.b_blue).getBackground();  
+    	PorterDuffColorFilter blue_filter = new PorterDuffColorFilter(Color.BLUE, PorterDuff.Mode.SRC_ATOP);
+        blue_bg.setColorFilter(blue_filter);
         red = (Button) findViewById(R.id.b_red);
         blue = (Button) findViewById(R.id.b_blue);
         red.setOnClickListener(myListener);
         blue.setOnClickListener(myListener);
     }
-    
+
     View.OnClickListener myListener = new View.OnClickListener() {
 
-		public void onClick(View v) {
+	public void onClick(View v) {
 			if(((Button)v).getVisibility() == View.INVISIBLE) {
 				((Button)v).setVisibility(View.VISIBLE);
 				if(((Button)v).getId() == red.getId()) {
@@ -44,8 +54,6 @@ public class AWoUSOActivity extends Activity {
 					red.setVisibility(View.VISIBLE);
 				}
 			}
-			
 		}
-        
     };
 }
