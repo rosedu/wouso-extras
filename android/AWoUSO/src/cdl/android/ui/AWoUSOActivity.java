4b26748df3ee58eceeb35cc69e2d2f06c96f6076
Dragos Badea <badea.dm@gmail.com>
2012-03-17 02:10:53 +0200

    Updated AWoUSOActivity.java

--- android/AWoUSO/src/cdl/android/ui/AWoUSOActivity.java
+++ android/AWoUSO/src/cdl/android/ui/AWoUSOActivity.java
@@ -3,14 +3,17 @@ package cdl.android.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.FrameLayout.LayoutParams;
+import android.widget.EditText;
+import android.widget.Toast;
 import cdl.android.R;
 
 public class AWoUSOActivity extends Activity {
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,16 +21,44 @@ public class AWoUSOActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
         
-        Button button = (Button) findViewById(R.id.buton_test);        
-        
-        final Intent bla = new Intent(this, NextActivity.class);
+        Button button = (Button) findViewById(R.id.loginButton);
+        final Intent mainM = new Intent(this, MainMenu.class);
+        final Toast loginNotification = Toast.makeText(getApplicationContext(), "Buffer", Toast.LENGTH_SHORT);
         
         button.setOnClickListener(new OnClickListener() {
 			
-			@Override
 			public void onClick(View v) {
-				startActivity(bla);
+				loginNotification.cancel();
+				EditText userBuffer = (EditText) findViewById(R.id.username);
+				String userName = userBuffer.getText().toString();
+				EditText passBuffer = (EditText) findViewById(R.id.password);
+		        String password = passBuffer.getText().toString();
+		        int clear = 1;
+
+
+				loginNotification.setGravity(Gravity.CENTER, 0, 0);
+		        
+				if ( userName.isEmpty() ){
+					loginNotification.setText("Introduceti numele de utilizator!");
+					clear = 0;
+				}
 				
+				if ( password.isEmpty() ){
+					if ( clear == 0 ){
+						loginNotification.setText("Introduceti numele de utilizator\n                    si parola!");
+					}
+					else {
+						loginNotification.setText("Introduceti parola!");
+						clear = 0;
+					}
+				}
+				
+				if (clear == 1){
+					startActivity(mainM);
+				}
+				else {
+					loginNotification.show();
+				}
 			}
 		});
     }
