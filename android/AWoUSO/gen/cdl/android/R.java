a4ab8856db90425847264b330ef9018945f6ccd1
Diana Mincu <diana.mincu@cti.pub.ro>
2012-06-20 12:04:11 +0300

    Created layout for Create message

--- android/AWoUSO/gen/cdl/android/R.java
+++ android/AWoUSO/gen/cdl/android/R.java
@@ -9,12 +9,14 @@ package cdl.android;
 
 public final class R {
     public static final class array {
-        public static final int list_array=0x7f050000;
+        public static final int list_array=0x7f060000;
     }
     public static final class attr {
     }
     public static final class color {
-        public static final int colors=0x7f070000;
+        public static final int colors=0x7f040002;
+        public static final int msg_button_black=0x7f040001;
+        public static final int white=0x7f040000;
     }
     public static final class drawable {
         public static final int androidicon=0x7f020000;
@@ -72,31 +74,33 @@ public final class R {
     public static final class layout {
         public static final int bazaar=0x7f030000;
         public static final int bazaar_list_item=0x7f030001;
-        public static final int exchange=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int mainmenu=0x7f030004;
-        public static final int profileoverlay=0x7f030005;
-        public static final int tab_layout=0x7f030006;
+        public static final int compose_msg_layout=0x7f030002;
+        public static final int exchange=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int mainmenu=0x7f030005;
+        public static final int profileoverlay=0x7f030006;
+        public static final int tab_layout=0x7f030007;
     }
     public static final class string {
-        public static final int account=0x7f040002;
-        public static final int app_name=0x7f040000;
-        public static final int chalMarker=0x7f040005;
-        public static final int exchange_left=0x7f04000a;
-        public static final int exchange_right=0x7f04000b;
-        public static final int loginText=0x7f040001;
-        public static final int logout=0x7f04000c;
-        public static final int msgMarker=0x7f040006;
-        public static final int passwd=0x7f040003;
-        public static final int qotdMarker=0x7f040004;
-        public static final int shopMarker=0x7f040007;
-        public static final int spQuestMarker=0x7f040009;
-        public static final int topMarker=0x7f040008;
+        public static final int account=0x7f050002;
+        public static final int app_name=0x7f050000;
+        public static final int chalMarker=0x7f050005;
+        public static final int create_msg=0x7f05000d;
+        public static final int exchange_left=0x7f05000a;
+        public static final int exchange_right=0x7f05000b;
+        public static final int loginText=0x7f050001;
+        public static final int logout=0x7f05000c;
+        public static final int msgMarker=0x7f050006;
+        public static final int passwd=0x7f050003;
+        public static final int qotdMarker=0x7f050004;
+        public static final int shopMarker=0x7f050007;
+        public static final int spQuestMarker=0x7f050009;
+        public static final int topMarker=0x7f050008;
     }
     public static final class style {
-        public static final int BlueButton=0x7f060000;
-        public static final int mainMenuButtons=0x7f060001;
-        public static final int profileFrame=0x7f060003;
-        public static final int specialButtons=0x7f060002;
+        public static final int BlueButton=0x7f070000;
+        public static final int mainMenuButtons=0x7f070001;
+        public static final int profileFrame=0x7f070003;
+        public static final int specialButtons=0x7f070002;
     }
 }
