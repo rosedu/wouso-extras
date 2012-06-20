c137ee85d21f8d16e9cdb79411765d0dd4218b19
Cosmin Stamate <cos.stamate@yahoo.com>
2012-06-20 14:40:12 +0300

    Add user info background image and action buttons

--- android/AWoUSO/src/cdl/android/ui/user/UserProfile.java
+++ android/AWoUSO/src/cdl/android/ui/user/UserProfile.java
@@ -1,11 +1,18 @@
 package cdl.android.ui.user;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.view.Gravity;
@@ -14,6 +21,7 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import cdl.android.R;
@@ -35,7 +43,39 @@ public class UserProfile extends Activity {
 	    super.onCreate(savedInstanceState);
 	    requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.user_profile);
-
+        
+        Button messageButton = (Button) findViewById(R.id.usermsgbtn);
+        Button spellButton = (Button) findViewById(R.id.userspellbtn);
+        Button challengeButton = (Button) findViewById(R.id.userchalbtn);
+        LinearLayout userInfoLayout = (LinearLayout) findViewById(R.id.usercontainer);
+        
+        
+        File sdcard = Environment.getExternalStorageDirectory();
+        File background = new File(sdcard + File.separator + "awouso" + File.separator +
+        		"profiles", "CA.png");
+        Bitmap backgroundBitmap = BitmapFactory.decodeFile(background.toString());
+        System.err.println(background);
+        @SuppressWarnings("deprecation")
+		Drawable backgroundImage = new BitmapDrawable(backgroundBitmap);
+        userInfoLayout.setBackgroundDrawable(backgroundImage);
+        
+        messageButton.setOnClickListener(new View.OnClickListener() {
+    		public void onClick(View v) {
+    			Toast.makeText(getApplicationContext(), "Send a message to ", Toast.LENGTH_SHORT).show();
+    		}
+    	});
+        
+        spellButton.setOnClickListener(new View.OnClickListener() {
+    		public void onClick(View v) {
+    			Toast.makeText(getApplicationContext(), "Cast a spell on ", Toast.LENGTH_SHORT).show();
+    		}
+    	});
+        
+        challengeButton.setOnClickListener(new View.OnClickListener() {
+    		public void onClick(View v) {
+    			Toast.makeText(getApplicationContext(), "Challenge ", Toast.LENGTH_SHORT).show();
+    		}
+    	});
 	}	
 
 }
