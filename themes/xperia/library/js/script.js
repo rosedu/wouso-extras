8d55d18252745f6ee4931214cc7ffb8d221fcbc7
piatra <andrei.br92@gmail.com>
2013-02-15 16:43:33 +0000

    new approach

--- themes/xperia/library/js/script.js
+++ themes/xperia/library/js/script.js
@@ -1,14 +1,19 @@
-$('h1 span').on('keydown', function (e) {
+document.querySelector('h1 span').addEventListener('keydown', function (e) {
 
-	var span = $(this);
-	if(span.text().trim() != '_') {
-		span.removeClass('blink');
+	var span = this;
+	
+	if(span.innerText.trim() != '_') {
+		span.classList.remove('blink');
 	}
 
 	if(e.keyCode == 13) {
-		$('<h1>' + span.text().substring(0,10) + ' : command not found </h1>').insertAfter(span);
+		var h1 = document.createElement('h1');
+		h1.innerHTML = span.innerText.substring(0, 10) + ' : command not found';
+		insertAfter(this.parentNode, h1);		
 	}
-
-
+	
 });
 
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
