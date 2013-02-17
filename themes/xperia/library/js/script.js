bca6bd99f698619658b938948a30ef7491fcf193
piatra <andrei.br92@gmail.com>
2013-02-17 12:38:43 +0000

    :bowtie:

--- themes/xperia/library/js/script.js
+++ themes/xperia/library/js/script.js
@@ -1,4 +1,10 @@
 var output = document.querySelector('#output');
+var nickname = document.querySelector('#nickname').dataset.name;
+var commands = {};
+commands.home = {
+	command : 'cd ~/home/' + nickname,
+	instruction : 'location.href = location.href.substring(0,27)'
+};
 
 document.querySelector('h1 span').addEventListener('keydown', function (e) {
 
@@ -10,23 +16,29 @@ document.querySelector('h1 span').addEventListener('keydown', function (e) {
 
 	if(e.keyCode == 13) {
 		var h1 = document.createElement('h1');
-		h1.innerHTML = span.innerText.substring(0, 10) + ' : command not found';
+
+		for(var i in commands) {
+			if(commands[i].command == span.innerText) {
+				eval(commands[i].instruction);
+				e.preventDefault();
+				return false;
+			}
+		}
+
+		h1.innerHTML = span.innerText + ' : command not found';
 		output.appendChild(h1);
-      	output.scrollTop = output.scrollHeight;
-      	span.innerText = '';
-      	e.preventDefault();
+		output.scrollTop = output.scrollHeight;
+		span.innerText = '';
+		e.preventDefault();
 	}
 	
 });
 
-document.querySelector('h1 span').addEventListener('click', function (e) {
-  	span = this;
-	if(span.innerText.trim() != '_') {
-      	span.innerText = '';
+document.querySelector('h1 span').addEventListener('click', function () {
+	var span = this;
+
+	if(span.innerText.trim() == '_') {
+		span.innerText = '';
 		span.classList.remove('blink');
 	}
 });
-
-function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
