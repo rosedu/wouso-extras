4740abbdd063d2015f2ca7f1e2bf414e31fce22d
Alex Eftimie <alex@eftimie.ro>
2013-03-22 16:22:19 +0200

    [chrome-notifier] Updated version, now works with woso 2013. Also bumped manifest to version 2.

---
+++ wouso-chrome-notifier/options.js
@@ -0,0 +1,24 @@
+function saveOptions() {
+    var bkg = chrome.extension.getBackgroundPage();
+    var select = document.getElementById("edition");
+    var edition = select.children[select.selectedIndex].value;
+    bkg.settings.edition = edition;
+    bkg.loadOptions();
+}
+
+function loadOptions() {
+    var bkg = chrome.extension.getBackgroundPage();
+    var select = document.getElementById("edition");
+    alert(bkg);
+    edition = bkg.settings.edition;
+
+    for (c=0; c<select.children.length; c++) {
+        if (select.children[c].value == edition)
+            select.children[c].selected = true;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('button').addEventListener('click', saveOptions);
+  loadOptions();
+});
