e091c1fe6cbc2e16dfa41fca52201abd200f6e75
Radu Cornel <radu_cornel92@yahoo.com>
2013-04-06 01:15:39 +0300

    Now the BazaarItem can return the full url of the spell image

--- android/AWoUSO/src/cdl/android/general/BazaarItem.java
+++ android/AWoUSO/src/cdl/android/general/BazaarItem.java
@@ -1,14 +1,24 @@
 package cdl.android.general;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+import cdl.android.R;
+import cdl.android.server.ApiHandler;
 
 public class BazaarItem {
 	private String name;
 	private String title;
 	private String type;
 	private int dueDays;
-	private String imagURL;
+	private String imageURL;
 	private int price;
 	private int groupId;
 	private int id;
@@ -19,10 +29,12 @@ public class BazaarItem {
 		title = jobj.getString("title");
 		type = jobj.getString("type");
 		dueDays = jobj.getInt("due_days");
-		imagURL = jobj.getString("image_url");
+		imageURL = "http://wouso-next.rosedu.org" + jobj.getString("image_url");
+		System.out.println(imageURL);
 		price = jobj.getInt("price");
 		description = jobj.getString("description");
 	}
+	
 	
 	public String getName(){
 		return this.name;
@@ -41,7 +53,7 @@ public class BazaarItem {
 	}
 	
 	public String getImageURL(){
-		return this.imagURL;
+		return this.imageURL;
 	}
 	
 	public String getPrice(){
