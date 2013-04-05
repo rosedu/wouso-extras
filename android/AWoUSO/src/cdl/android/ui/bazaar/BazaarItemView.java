d4952a23d2676c9a876cb2a4104988d128a800b0
Radu Cornel <radu_cornel92@yahoo.com>
2013-04-06 01:16:35 +0300

    Setting the spell image

--- android/AWoUSO/src/cdl/android/ui/bazaar/BazaarItemView.java
+++ android/AWoUSO/src/cdl/android/ui/bazaar/BazaarItemView.java
@@ -1,5 +1,8 @@
 package cdl.android.ui.bazaar;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,9 +10,13 @@ import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,7 +38,10 @@ public class BazaarItemView extends LinearLayout {
 		LayoutInflater layoutInflater = (LayoutInflater) getContext()
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		layoutInflater.inflate(R.layout.bazaar_list_item, this, true);
-
+		
+		ImageView spellImage = (ImageView) findViewById(R.id.spell_image);
+		setSpellImage(spellImage, item.getImageURL());
+		
 		TextView title = (TextView) findViewById(R.id.title);
 		title.setText(item.getTitle());
 
@@ -60,6 +70,24 @@ public class BazaarItemView extends LinearLayout {
 			}
 		});
 
+	}
+	
+	private static void setSpellImage(ImageView img, String url) {
+
+		Bitmap b = BitmapFactory.decodeResource(img.getContext()
+				.getResources(), R.drawable.empty);
+		img.setImageBitmap(b);
+		try {
+			HttpURLConnection con = (HttpURLConnection) (new URL(url))
+					.openConnection();
+			con.connect();
+			b = BitmapFactory.decodeStream(con.getInputStream());
+			img.setImageBitmap(b);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
