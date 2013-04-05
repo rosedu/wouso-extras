045119cbf6e41535b50b08dd21db974fca6d0b10
Radu Cornel <radu_cornel92@yahoo.com>
2013-04-06 00:19:09 +0300

    I've removed setAll method and I've added getImageURL

--- android/AWoUSO/src/cdl/android/general/SummaryItem.java
+++ android/AWoUSO/src/cdl/android/general/SummaryItem.java
@@ -23,43 +23,8 @@ public class SummaryItem {
 		spellName = jobj.getString("spell_name");
 		spellTitle = jobj.getString("spell_title");
 		spellId = jobj.getInt("spell_id");
-	}
-	
-	public void setAll(Context context){
-		ArrayList<BazaarItem> mItems = new ArrayList();
-		
-		ServerResponse resp = ApiHandler.get(ApiHandler.baseURL + "bazaar/", context);
-		
-		if(resp.getStatus() == false){
-		
-			Toast.makeText(context, resp.getError() , Toast.LENGTH_SHORT).show();
-		
-		}else{
-			try {
-				JSONArray spellData = (JSONArray) ((JSONObject)resp.getData()).get("spells");
-			
-				System.out.println(spellData.length());
-				for(int i = 0; i < spellData.length(); i++){
-					BazaarItem bazaarItem = new BazaarItem();
-					
-					try{
-						bazaarItem.parseContent(spellData.getJSONObject(i));
-						mItems.add(bazaarItem);
-						
-					}catch(JSONException e){
-						Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
-					}
-				}
-			}catch(JSONException e){
-				e.printStackTrace();
-			}
-		}
-		
-		for(BazaarItem item : mItems){
-			if(item.getTitle().equals("" + spellTitle)){
-				imageURL = item.getImageURL();
-			}
-		}
+		imageURL = jobj.getString("image_url");
+		amount = jobj.getInt("amount");
 	}
 	
 	public String getAmount(){
