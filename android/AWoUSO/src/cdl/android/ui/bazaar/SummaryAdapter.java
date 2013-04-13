6aa9d80af984fcc052e2069ebab837ff12f35299
Radu Cornel <radu_cornel92@yahoo.com>
2013-04-13 22:26:13 +0300

    Constructor in summaryAdapter receives Bundle instead of onClickListener. I've also added castSpell when item is clicked

--- android/AWoUSO/src/cdl/android/ui/bazaar/SummaryAdapter.java
+++ android/AWoUSO/src/cdl/android/ui/bazaar/SummaryAdapter.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import cdl.android.general.BazaarItem;
 import cdl.android.general.SummaryItem;
 import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
@@ -14,10 +16,12 @@ public class SummaryAdapter extends BaseAdapter{
 	private ArrayList<SummaryItem> mItems;
 	private OnClickListener mOnItemClick;
 	private Context mContext;
+	private Bundle mArgs;
 	
-	public SummaryAdapter(Context context, ArrayList<SummaryItem> items, OnClickListener onItemClick) {
+	
+	public SummaryAdapter(Context context, ArrayList<SummaryItem> items, Bundle args) {
 		mItems = new ArrayList<SummaryItem>();
-		mOnItemClick = onItemClick;
+		mArgs = args;
 		mContext = context;
 		mItems = (ArrayList<SummaryItem>) items.clone();
 	}
@@ -36,11 +40,19 @@ public class SummaryAdapter extends BaseAdapter{
 	}
 
 	@Override
-	public View getView(int index, View convertView, ViewGroup parent) {
+	public View getView(final int index, View convertView, ViewGroup parent) {
 		SummaryItemView item;
 
 		item = new SummaryItemView(mContext, mItems.get(index));
-		item.setOnClickListener(mOnItemClick);
+		item.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v) {
+				String action = mArgs.getString("action");
+				if(action.equals("castspell")){
+					int playerId = mArgs.getInt("playerID");
+					SummaryFragment.castSpell(playerId, mItems.get(index).getId(), mContext);
+				}
+				
+			}});
 		item.setClickable(true);
 
 		return item;
