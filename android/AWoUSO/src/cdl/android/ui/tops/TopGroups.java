b062200eb86bda9e8677b5fbf2c5824af766c0f5
cornel <cornel@rapture.(none)>
2013-04-18 01:18:09 +0300

    I have completed the TopGroups fragment java source file, setting up it's list adapter

--- android/AWoUSO/src/cdl/android/ui/tops/TopGroups.java
+++ android/AWoUSO/src/cdl/android/ui/tops/TopGroups.java
@@ -1,31 +1,71 @@
 package cdl.android.ui.tops;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+
 import cdl.android.R;
+import cdl.android.general.ServerResponse;
+import cdl.android.server.ApiHandler;
 import android.support.v4.app.*;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class TopGroups extends Fragment{
-
+	ArrayList<TopGroupsItem> groupsItems;
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
 			
-			//View view = new View(this.getActivity());
-			View view =  inflater.inflate(R.layout.top_groups, container, false);
-			
-			TextView textView = (TextView) view.findViewById(R.id.basic_aux);
-			textView.setTextColor(Color.BLUE);
-			textView.setText("Top Groups is not ready yet");
-			
+		View view =  inflater.inflate(R.layout.top_races, container, false);
 		
-			
-			
-			return view;
+		ListView mListView = (ListView) view.findViewById(R.id.top_races_list);
+		mListView.setEmptyView(view.findViewById(android.R.id.empty));
+		
+		
+		
+		groupsItems = new ArrayList<TopGroupsItem>();
+		
+		ServerResponse resp = ApiHandler.getArray("http://wouso-next.rosedu.org/api/top/group/", view.getContext());
+		
+		
+		if(resp.getStatus() == false){
+		
+			Toast.makeText(view.getContext(), resp.getError() , Toast.LENGTH_SHORT).show();
+		
+		}else{
+			try {
+				JSONArray topGroupsData = (JSONArray) resp.getArrayData();
+				
+				
+				for(int i = 0; i < topGroupsData.length(); i++){
+					TopGroupsItem groupsItem = new TopGroupsItem();
+					
+					try{
+						groupsItem.parseContent(topGroupsData.getJSONObject(i));
+						groupsItem.setPlace(i + 1);
+						groupsItems.add(groupsItem);
+						
+					}catch(Exception e){
+						Toast.makeText(view.getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+					}
+				}
+			}catch(Exception  e){
+				e.printStackTrace();
+			}
+		}
+	
+		
+		mListView.setAdapter(new TopGroupsAdapter(this.getActivity().getApplicationContext(), groupsItems));
+		
+		
+		return view;
 	}
 }
